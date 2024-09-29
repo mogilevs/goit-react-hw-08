802562e0e1eb845167e006df356bca9e0f7295aa
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-09-29 20:56:24 +0300

    remove console logs

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -14,11 +14,8 @@ const clearAuthHeader = () => {
 export const register = createAsyncThunk(
   "auth/register",
   async (credentials, thunkAPI) => {
-    console.log(credentials);
-
     try {
       const response = await axios.post("/users/signup", credentials);
-      console.log(response);
       setAuthHeader(response.data.token);
       return response.data;
     } catch (error) {
